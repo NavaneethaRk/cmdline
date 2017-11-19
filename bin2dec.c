3e654b6c2bf6ba2fc1bcf3f56dbc7ee1e7fc2784
NavaneethaRk <30334357+NavaneethaRk@users.noreply.github.com>
2017-11-19 16:30:47 +0530

    bin2dec.c

--- bin2dec.c
+++ bin2dec.c
@@ -2,15 +2,15 @@
 #include<stdlib.h>
 void main(int argc,char *argv[])
 {
-int num,rem,base=1,bin,dec=0;
+int num,rem,pos=1,bin,dec=0;
 bin=atoi(argv[1]);
 num=bin;
 while(num>0)
 {
 rem=num%10;
-dec=dec+rem*base;
+dec=dec+rem*pos;
 num=num/10;
-base=base*2;
+pos=pos*2;
 }
 printf("%d",dec);
 }
